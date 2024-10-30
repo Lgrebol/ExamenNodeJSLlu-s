45278f1675ff7b82b26b9c92e3195e112a7e9cda
Lgrebol <lgrebol08@boscdelacoma.cat>
2024-10-30 20:01:28 +0100

    Tasca 1 quasi feta 20:00

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-
+const fs = require('fs');
 app.use(express.static(path.join(__dirname, 'public')));
+const jsonFile = path.join(__dirname, 'productes.json');
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
@@ -12,3 +13,23 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
+
+//Tasca 1 Llegir el fitxer JSON i retornar un array d'objectes
+
+function llegirProductes() {
+  return new Promise((resolve, reject) => {
+    fs.readFile(jsonFile, 'utf8', (err, data) => {
+      if (err) {
+        reject(err);
+      } else {
+        try {
+          const games = JSON.parse(data);
+          resolve(games);
+        } catch (error) {
+          reject(error);
+        }
+      }
+    });
+  });
+}
+
